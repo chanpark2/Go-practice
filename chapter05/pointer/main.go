62177962408db4d69ce0d42cf2a93ad42bab2f80
chanpark2 <chpark@witchcompany.io>
2023-08-02 16:09:33 +0900

    feat: 포인터

---
+++ chapter05/pointer/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type Data struct {
+	value int
+	data  [200]int
+}
+
+type User struct {
+	Name string
+	Age  int
+}
+
+func NewUser(name string, age int) *User {
+	return &User{name, age}
+}
+
+func ChangeData(arg *Data) {
+	(*arg).value = 999
+	(*arg).data[100] = 999
+}
+func main() {
+	var a int = 10
+	var pa *int = &a
+
+	fmt.Println(pa)
+	fmt.Println(*pa)
+
+	*pa = 20
+	fmt.Println(a)
+
+	var p1 *int = &a
+	var p2 *int = &a
+	fmt.Println(p1 == p2)
+
+	var data Data
+	ChangeData(&data)
+	fmt.Println("value:", data.value)
+	fmt.Println("data[100]:", data.data[100])
+
+	userPointer := NewUser("AAA", 23)
+	fmt.Println(userPointer)
+}
